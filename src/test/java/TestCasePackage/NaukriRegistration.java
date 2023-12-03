4684fb0d318ab38a160c1e8c391a1c6af6f1bb4b
gauravpawan89 <gauravabsas1234567@gmail.com>
2023-12-03 18:32:38 +0530

    pushing code to branch

--- src/test/java/TestCasePackage/NaukriRegistration.java
+++ src/test/java/TestCasePackage/NaukriRegistration.java
@@ -2,20 +2,26 @@ package TestCasePackage;
 
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import BasePackage.BaseClass;
+import PageFactory.RegistrationPage;
 import UtilityPackage.ConfigUtility;
 import UtilityPackage.ConstantPaths;
+import UtilityPackage.ReadUtility;
 import UtilityPackage.WaitUtility;
 
 public class NaukriRegistration extends BaseClass {
-	
+
 	ConfigUtility con;
 	String fPath = ConstantPaths.fPath;
-	
+	String exPath = ConstantPaths.excelPath;
+	RegistrationPage rp;
+	ReadUtility ru;
+
 	@Test(priority = 1, description = "Wait for Logo")
-	public void carrerRegistration() {
+	public void logoVerified() {
 		con = new ConfigUtility(fPath);
 		getDriver().get(con.getUrl());
 		log.info("Naukri Registration page is launched");
@@ -23,11 +29,41 @@ public class NaukriRegistration extends BaseClass {
 		WebElement naukriLogo = WaitUtility.waitForImage(getDriver());
 		log.info("Wait for Image to be loaded");
 		System.out.println("Element is..." + naukriLogo);
-		boolean status =naukriLogo.isDisplayed();
+		boolean status = naukriLogo.isDisplayed();
 		log.info("Image is verified and Displayed");
 		Assert.assertTrue(status);
 	}
-	
-	
-	
+
+	@Test(dataProvider = "rData", priority = 2)
+	public void carrerRegis(String n, String e, String p, String m) {
+		rp = new RegistrationPage(getDriver());
+		rp.getName(n);
+		rp.getEmail(e);
+		rp.getPassword(p);
+		rp.getMobile(m);
+
+	}
+
+	@DataProvider(name = "rData")
+	public Object[][] regisDataProvider() {
+		ru = new ReadUtility(exPath, "sheet1");
+
+		int r = ru.countRows();
+		int c = ru.countColumns(r);
+
+		String[][] arr = new String[r][c];
+
+		for (int i = 1; i <= r; i++) {
+
+			for (int j = 0; j < c; j++) {
+
+				arr[i - 1][j] = ru.getData(i, j);
+
+			}
+
+		}
+		return arr;
+
+	}
+
 }
--- src/test/java/UtilityPackage/ConstantPaths.java
+++ src/test/java/UtilityPackage/ConstantPaths.java
@@ -3,5 +3,6 @@ package UtilityPackage;
 public class ConstantPaths {
 	
 	public static String fPath = "./Config/Configp.properties";
+	public static String excelPath = "./ExternalFiles/ExcelRead.xlsx";
 
 }
